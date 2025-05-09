f6f1ad4fd1324f2c4d7ffba6f531d97179b40d3c
OmarMuita <omarmuita@gmail.com>
2025-05-09 19:37:47 +0300

    Add files via upload

---
+++ CHARACTER CHECKER BOIII.py
@@ -0,0 +1,9 @@
+
+def is_alphabet(char):
+    return char.isalpha()
+
+char=input("Input your charcter to check if its an alphabet:")
+if char.isalpha:
+    print("Your character is an alphabet")
+else:
+    print("It is not an alphabet")
